f71d9c9aedc284b8bc1d7bad103b70caa12971d5
Ankit Karnany <ankitkarnany@gmail.com>
2023-11-17 15:53:54 +0100

    chore: empty nuxt page

---
+++ components/hero.vue
@@ -0,0 +1,11 @@
+<!-- eslint-disable max-len -->
+<template>
+  <div>
+    Hero
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss" scoped>
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="main">
+    <hero></hero>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
