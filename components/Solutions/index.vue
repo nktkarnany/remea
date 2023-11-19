82c151bba17b7d5046a55cc1e92e3ee64da0950f
Ankit Karnany <ankitkarnany@gmail.com>
2023-11-19 16:45:49 +0100

    chore: solutions styling implemented

--- components/Solutions/index.vue
+++ components/Solutions/index.vue
@@ -7,25 +7,29 @@
           <template #title>{{ $t('solution-1-title') }}</template>
           <template #text>
             <p>{{ $t('solution-1-sub-title') }}</p>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
-              </template>
-              <template #text> {{ $t('solution-1-item-1') }} </template>
-            </icon-text>
-            <icon-text>
-              <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
               <template #text>
-                {{ $t('solution-1-item-2') }}
+                <p>{{ $t('solution-1-item-1') }}</p>
               </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-1-item-3') }} </template>
+              <template #text>
+                <p>{{ $t('solution-1-item-2') }}</p>
+              </template>
+            </icon-text>
+            <icon-text class="mb-5">
+              <template #icon>
+                <NuxtImg src="/check-icon-sm.svg" />
+              </template>
+              <template #text>
+                <p>{{ $t('solution-1-item-3') }}</p>
+              </template>
             </icon-text>
           </template>
         </multiple-content>
@@ -38,29 +42,37 @@
           <template #title>{{ $t('solution-2-title') }}</template>
           <template #text>
             <p>{{ $t('solution-2-sub-title') }}</p>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-2-item-1') }} </template>
+              <template #text>
+                <p>{{ $t('solution-2-item-1') }}</p>
+              </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-2-item-2') }} </template>
+              <template #text>
+                <p>{{ $t('solution-2-item-2') }}</p>
+              </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-2-item-3') }} </template>
+              <template #text>
+                <p>{{ $t('solution-2-item-3') }}</p>
+              </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-2-item-4') }} </template>
+              <template #text>
+                <p>{{ $t('solution-2-item-4') }}</p>
+              </template>
             </icon-text>
           </template>
         </multiple-content>
@@ -73,29 +85,37 @@
           <template #title>{{ $t('solution-3-title') }}</template>
           <template #text>
             <p>{{ $t('solution-3-sub-title') }}</p>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-3-item-1') }} </template>
+              <template #text>
+                <p>{{ $t('solution-3-item-1') }}</p>
+              </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-3-item-2') }} </template>
+              <template #text>
+                <p>{{ $t('solution-3-item-2') }}</p>
+              </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-3-item-3') }} </template>
+              <template #text>
+                <p>{{ $t('solution-3-item-3') }}</p>
+              </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-3-item-4') }} </template>
+              <template #text>
+                <p>{{ $t('solution-3-item-4') }}</p>
+              </template>
             </icon-text>
           </template>
         </multiple-content>
@@ -108,23 +128,29 @@
           <template #title>{{ $t('solution-4-title') }}</template>
           <template #text>
             <p>{{ $t('solution-4-sub-title') }}</p>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-4-item-1') }} </template>
+              <template #text>
+                <p>{{ $t('solution-4-item-1') }}</p>
+              </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-4-item-2') }} </template>
+              <template #text>
+                <p>{{ $t('solution-4-item-2') }}</p>
+              </template>
             </icon-text>
-            <icon-text>
+            <icon-text class="mb-5">
               <template #icon>
-                <NuxtImg src="/check-icon.svg" />
+                <NuxtImg src="/check-icon-sm.svg" />
               </template>
-              <template #text> {{ $t('solution-4-item-3') }} </template>
+              <template #text>
+                <p>{{ $t('solution-4-item-3') }}</p>
+              </template>
             </icon-text>
           </template>
         </multiple-content>
